083cc3bd0f5b416a758dd009f6d3e5ea9f8c08d6
Wonseok Ryu <77234845+johninkorea@users.noreply.github.com>
2022-02-18 17:48:27 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,17 +2,13 @@
 
 cd ~
 
-
 mkdir .rws
 cp ./lab_share/program/* ./.rws
-
-
+echo "program install done"
 
 cat ./lab_share/alias_list
 cat ./lab_share/alias_list >> .bashrc
+echo "shortcut keys uodate done"
 
-source .bashrc
-
-
-
-
+source ~/.bashrc
+echo "install successfully done"

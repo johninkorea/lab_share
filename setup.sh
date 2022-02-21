342b90b2b8a193763b93b8c48eedf915db980310
Wonseok Ryu <77234845+johninkorea@users.noreply.github.com>
2022-02-21 18:39:34 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,18 +4,11 @@ PATH=`pwd -P`
 cd ~
 
 mkdir .rws
-# cp ./lab_share/program/* ./.rws
-cp $PATH/program/* ~/.rws
+cp ./lab_share/program/* ./.rws
 echo "program install done"
 
-#cat ./lab_share/alias_list
-#cat ./lab_share/alias_list >> .bashrc
-
-cat $PATH/alias_list
-cat $PATH/alias_list >> .bashrc
-
-
-
+cat ./lab_share/alias_list
+cat ./lab_share/alias_list >> .bashrc
 echo "shortcut keys uodate done"
 
 source ~/.bashrc
